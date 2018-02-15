33cb405b29eae13ccd49bfdf2f3795aa21f66db1
Elchin Hasiev <hasiev.elchin98@gmail.com>
2018-02-15 15:22:56 +0200

    message

---
+++ css/style.cs
@@ -0,0 +1,48 @@
+html {
+  font-size: 16px; }
+
+body {
+  font-style: normal;
+  font-size: 1rem;
+  line-height: 24px;
+  font-family: 'PT Sans', sans-serif;
+  color: #978585;
+  background-color: #887467; }
+
+img {
+  max-width: 100%;
+  height: auto; }
+
+.visually-hidden {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  border: 0;
+  clip: rect(0 0 0 0);
+  overflow: hidden; }
+
+.btn {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  display: block;
+  padding: 15px 0;
+  text-decoration: none;
+  text-transform: uppercase;
+  text-align: center;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 24px;
+  color: #000;
+  border: 1px solid #e5ddd5;
+  transition-property: border-color, background-color, color;
+  transition-duration: .3s; }
+  .btn:hover {
+    border-color: #d5c9bd; }
+  .btn:active {
+    color: #958c84;
+    background-color: #d5c9bd; }
+
+/*# sourceMappingURL=style.cs.map */
